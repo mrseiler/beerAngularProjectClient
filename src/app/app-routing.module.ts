d0babb6d0b01c8490800298d6cb0206e13762a05
Austin Rardin <ajeflaunch@gmail.com>
2018-07-23 13:40:01 -0600

    styling changes and added home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Route, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DrinkersSignupComponent } from './auth/drinkers-signup/drinkers-signup.component';
+import { DrinkersLoginComponent } from './auth/drinkers-login/drinkers-login.component';
 
 //using children helps auth figure out what children it has 'can be used later for finishing up'
 const routes: Route[] = [ 
   { path: 'auth', component: AuthComponent },
+  { path: 'login', component: DrinkersLoginComponent},
   { path: '', redirectTo:'/auth', pathMatch: 'full' },
   { path: '**', component:PageNotFoundComponent },
 ];
