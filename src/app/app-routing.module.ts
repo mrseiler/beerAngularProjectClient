e994b3b5e8daf8b4b1052ac6144f46e9104e12cd
Austin Rardin <ajeflaunch@gmail.com>
2018-07-24 14:27:41 -0600

    major overhaul

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,17 +3,17 @@ import { Route, RouterModule } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { DrinkersLoginComponent } from './auth/drinkers-login/drinkers-login.component';
-import { HomeComponent } from './home/home.component';
 import { DrinkersSignupComponent } from './auth/drinkers-signup/drinkers-signup.component';
 import { AuthGuardService as AuthGuard } from './services/auth-guard.service';
 import { BeersComponent } from './beers/beers.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 //using children helps auth figure out what children it has 'can be used later for finishing up'
 const routes: Route[] = [ 
   { path: 'auth', component: AuthComponent },
   { path: 'signup', component: DrinkersSignupComponent },
   { path: 'login', component: DrinkersLoginComponent},
-  { path: 'home', component: HomeComponent},
+  { path: 'home', component: DashboardComponent},
   { path: 'beer', component: BeersComponent},
   { path: '', redirectTo:'/auth', pathMatch: 'full' },
   { path: '**', component:PageNotFoundComponent },
