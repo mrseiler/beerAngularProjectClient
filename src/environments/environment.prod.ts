4ecf1c3624abef060e73ef4e3e6fa250cf556d12
jfjeffries <jon.f.jeffries@gmail.com>
2018-07-31 22:27:21 -0400

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: true
 };
 
-export let APIURL = "https://holdmybeerclient.herokuapp.com";
+export let APIURL = "https://holdmybeerapi.herokuapp.com";
 // switch (window.location.hostname) {
 //   case 'holdmybeerclient.herokuapp.com':
-//     APIURL='https://holdmybeerclient.herokuapp.com'
+//     APIURL='https://holdmybeerapi.herokuapp.com'
 //     break;
 
 //   default:
