3859e7a0052440d26169bd2ed2507e9c663259c7
jfjeffries <jon.f.jeffries@gmail.com>
2018-08-01 09:26:59 -0400

    fix

--- src/server.js
+++ src/server.js
@@ -4,11 +4,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/holdmybeer'));
+app.use(express.static(__dirname + '/dist/holdmybeerclient'));
 
 app.get('*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/holdmybeer/index.html'));
+res.sendFile(path.join(__dirname+'/dist/holdmybeerclient/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
